b33c4aa71014edafccff400264c27f8a95787492
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-02-16 09:02:58 +0530

    Added solution - LeetHub

--- Good Stones - GFG/good-stones.cpp
+++ Good Stones - GFG/good-stones.cpp
@@ -7,71 +7,45 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 class Solution{
-    bool checkNode(vector<int> &arr, vector<int> &isSafe, int n, int i)
-    {
-        if (i < 0 || i >= n)
-        {
-            return true;
-        }
-
-        // looping
-        if(isSafe[i] == 2)
-        {
-            return false;
-        }
-
-        // already determined
-        if (isSafe[i] != -1)
-        {
-            return isSafe[i];
-        }
-
-        // case 1 : arr[i] = 0
-        if (arr[i] == 0)
-        {
-            isSafe[i] = 0;
-            return false;
-        }
-
-        // case 2 : move karo
-
-        // inhinit looping
-        isSafe[i] = 2;
-        bool canMove = checkNode(arr, isSafe, n, i + arr[i]);
-
-        if (canMove)
-        {
-            isSafe[i] = 1;
-            return true;
-        }
-
-        isSafe[i] = 0;
+public:
+bool check(int n,vector<int>&arr,vector<int>&issafe,int i){
+    if(i<0 || i>=n){
+        return true;
+    }
+    
+    if(issafe[i]==2){
         return false;
     }
-
-public:
-    int goodStones(int n, vector<int> &arr)
-    {
-        vector<int> isSafe(n, -1);
-
-        for (int i = 0; i < n; i++)
-        {
-            if (isSafe[i] == -1)
-            {
-                checkNode(arr, isSafe, n, i);
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (isSafe[i] == 1)
-            {
-                ans++;
-            }
-        }
-
-        return ans;
+    if(issafe[i]!=-1){
+        return issafe[i];
+    }
+    if(arr[i]==0){
+        issafe[i]=0;
+        return false;
+    }
+    issafe[i]=2;
+    bool canmove=check(n,arr,issafe,i+arr[i]);
+    if(canmove){
+        issafe[i]=1;
+        return true;
+    }
+    issafe[i]=0;
+    return false;
+}
+    int goodStones(int n,vector<int> &arr){
+   vector<int>issafe(n,-1);
+   for(int i=0;i<n;i++){
+       if(issafe[i]==-1){
+           check(n,arr,issafe,i);
+       }
+   }
+   int ans=0;
+   for(int i=0;i<n;i++){
+       if(issafe[i]==1){
+           ans++;
+       }
+   }
+   return ans;
     }  
 };
 
