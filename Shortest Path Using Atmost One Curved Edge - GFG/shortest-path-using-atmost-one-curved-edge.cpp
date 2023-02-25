5d7810cac4a8401793fc21c20650c22715a336fb
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-02-25 09:31:17 +0530

    Added solution - LeetHub

--- Shortest Path Using Atmost One Curved Edge - GFG/shortest-path-using-atmost-one-curved-edge.cpp
+++ Shortest Path Using Atmost One Curved Edge - GFG/shortest-path-using-atmost-one-curved-edge.cpp
@@ -8,73 +8,64 @@ using namespace std;
 
 class Solution {
   public:
-    int shortestPath(int n, int m, int a, int b, vector<vector<int>> &edges) {
-             priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-
-        vector<pair<int,int>>v1(n+1,{INT_MAX,INT_MAX});
-
-        v1[a]={0,0};
-
-        vector<pair<int,pair<int,int>>> adj1[n+1];
-
+  vector<int> dijkstra(int a,int b,int n,vector<pair<int,int>> adj[])
+    {
+        vector<int> dist(n+1,1e9);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        dist[a]=0;
+        pq.push({0,a});
         
-
-        for(int i=0;i<m;i++)
-
-        {
-
-            adj1[edges[i][0]].push_back({edges[i][1], {edges[i][2], edges[i][3]}}); 
-
-            adj1[edges[i][1]].push_back({edges[i][0], {edges[i][2], edges[i][3]}}); 
-
-        } 
-
-        pq.push({0, a});
-
-        
-
         while(!pq.empty())
-
         {
-
-            auto temp = pq.top();
-
+            auto curr=pq.top();
             pq.pop();
-
-             for(auto it : adj1[temp.second])
-
-             {
-
-                 if(it.first == temp.second) continue;
-
-                 int t = temp.first + it.second.first;
-
-                 int t1 = min( v1[temp.second].second + it.second.first, v1[temp.second].first + it.second.second);
-
-                 if(v1[it.first].first > t || v1[it.first].second > t1)
-
-                 {
-
-                    v1[it.first].first = min(v1[it.first].first, t);
-
-                    v1[it.first].second = min(v1[it.first].second, t1);
-
-                    pq.push({t, it.first});
-
-                 }
-
-             }
-
-             
-
+            int dis=curr.first;
+            int node=curr.second;
+            
+            for(auto it:adj[node])
+            {
+                int v=it.first;
+                int wt=it.second;
+                if(dist[v]>dist[node]+wt)
+                {
+                    dist[v]=dist[node]+wt;
+                    pq.push({dist[v],v});
+                }
+            }
         }
-
-        if(v1[b].first == INT_MAX) return -1;
-
-        return min(v1[b].first,v1[b].second);
-
+        return dist;
     }
-    
+    int shortestPath(int n, int m, int a, int b, vector<vector<int>> &edges) {
+       vector<pair<int,int>>adj[n+1];
+       vector<vector<int>>curved;
+       for(int i=0;i<m;i++){
+          int u=edges[i][0];
+            int v=edges[i][1];
+            int wt=edges[i][2];
+            int cwt=edges[i][3];
+            
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+            
+            curved.push_back({u,v,cwt});
+       }
+       vector<int> dist1=dijkstra(a,b,n,adj);
+        vector<int> dist2=dijkstra(b,a,n,adj);
+        int ans=dist1[b];
+        
+        for(int i=0;i<m;i++)
+        {
+            int u=curved[i][0];
+            int v=curved[i][1];
+            int cwt=curved[i][2];
+            
+            ans=min(ans,dist1[u]+cwt+dist2[v]);
+            ans=min(ans,dist1[v]+cwt+dist2[u]);
+        }
+        
+        if(ans>=1e9) return -1;
+        return ans;
+    }
 };
 
 //{ Driver Code Starts.
