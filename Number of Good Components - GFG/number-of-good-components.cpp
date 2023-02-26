253ad4d544395a763c59e1728a26ec6d33bbadd3
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-02-26 11:11:56 +0530

    Added solution - LeetHub

--- Number of Good Components - GFG/number-of-good-components.cpp
+++ Number of Good Components - GFG/number-of-good-components.cpp
@@ -8,74 +8,44 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-
-   class DisjointSet {
-    vector<int> rank, parent, size; 
-public: 
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
-        parent.resize(n+1);
-        size.resize(n+1); 
-        for(int i = 0;i<=n;i++) {
-            parent[i] = i; 
-            size[i] = 1; 
-        }
-    }
-
-    int find(int node) {
-        return node==parent[node]?node:parent[node]=find(parent[node]);
-    }
-
-    void unite(int u, int v) {
-        int ulp_u = find(u); 
-        int ulp_v = find(v); 
-        if(ulp_u == ulp_v) return; 
-        if(size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-            size[ulp_v] += size[ulp_u]; 
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v]; 
-        }
-    }
-}; 
-
 class Solution {
   public:
+    void dfs(int node,vector<int>&vis,vector<vector<int>>&adj,int &vertices,
+  int &edges)
+  {
+      vertices++;
+      vis[node]=1;
+  
+      for(auto i:adj[node])
+      {
+          edges++;
+          if(!vis[i])
+          dfs(i,vis,adj,vertices,edges);
+      }
+  }
     int findNumberOfGoodComponent(int V, vector<vector<int>>& adj) {
-        int n=adj.size()-1;
-        DisjointSet dsu(n);
-        for(int i=0;i<adj.size();i++){
-            for(auto dest:adj[i]){
-                dsu.unite(i,dest);
-            }
-        }
-        // for(auto src:adj){
-        //     for(auto dest:src)dsu.unite(src,dest);
-        // }
-        map <int,vector<int>> mp;
-        for(int i=1;i<=n;i++){
-            mp[dsu.find(i)].push_back(i);
-        }
-        // set <int> s;
-        int count=0;
-        for(auto it:mp){
-            
-            int src=it.first;
-            int temp=mp[src].size();
-            bool flag=true;
-            for(auto dest:mp[src]){
-                if((adj[dest].size()+1)!=temp){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)++count;
-        }
-        return count;
+        // code here
+       
+       
+        vector<int>vis(V+1,0);
         
+        int ans=0;
+        int i;
+        for(i=1;i<=V;i++)
+        {
+            //cout<<i<<endl;
+            if(!vis[i])
+            {
+                int edges=0;
+                int vertices=0;
+                dfs(i,vis,adj,vertices,edges);
+                //cout<<c<<" "<<sum<<endl;
+                if(vertices*(vertices-1)==edges) ans++;
+            }
+        }
+        return ans;
     }
+
 };
 
 //{ Driver Code Starts.
