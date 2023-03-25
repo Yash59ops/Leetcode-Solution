7a155015f1b74407e1b3030846f880c6eacf7f69
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-03-25 17:03:05 +0530

    Added solution - LeetHub

---
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+/*You are required to complete this method*/
+  int f(int i,int j, string &s,string &p,vector<vector<int>> &dp)
+    {
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==0 && j==0) return dp[i][j]=1;
+        if(j==0 && i>0) return dp[i][j]=0;
+        if(i==0 && j>0)
+        {
+            while(j>0)
+            {
+                if(p[j-1]=='*') j--;
+                else return dp[i][j]=0;
+            }
+            return dp[i][j]=1;
+        }
+        
+        if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
+        
+        if(p[j-1]=='*')
+        {
+            return dp[i][j] = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp) ? 1:0;
+            //Two cases
+            //Consider * as len=0
+            //Give one charcter to * and remain at *
+            //at next step it will again be decided from both these cases
+        }
+        return dp[i][j]=0;
+        
+    }
+    int wildCard(string p,string s)
+    {
+         int n=s.length(),m=p.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,s,p,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+           string pat,text;
+           cin>>pat;
+cin.ignore(numeric_limits<streamsize>::max(),'\n');
+           cin>>text;
+           Solution obj;
+           cout<<obj.wildCard(pat,text)<<endl;
+   }
+}
+
+// } Driver Code Ends
