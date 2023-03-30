a9a362869cc49261f4dcf5e6c44a13f9e686b900
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-03-30 09:16:47 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -28,7 +28,7 @@ int solve(int i,int j,int arr[],vector<vector<int>>&dp){
        for(int i=0;i<N;i++)
        dp[i][i]=0;
        for(int i=N-1;i>=1;i--){
-          for(int j=i+1;j<=N-1;j++){
+          for(int j=i+1;j<=N-1;j++){//kyunki j hamesha i ke right me hoga isiliye i+1 se start kiya he
               int mini=INT_MAX;
             for(int k=i;k<=j-1;k++){
                 int cost=dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
