a9103c2b7f449af7f4ad7d43a5b711d9744f8980
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-05-14 12:51:47 +0530

    Added solution - LeetHub

--- Maximum subset sum - GFG/maximum-subset-sum.cpp
+++ Maximum subset sum - GFG/maximum-subset-sum.cpp
@@ -31,71 +31,29 @@ public:
 class Solution {
 
   public:
-   // long long solveRec(int n, vector<int> &a){
-    //     if(n<0) return 0;
-    //     if(n==0) return a[n];
-        
-    //     long long one = solveRec(n-1, a);
-    //     long long two = solveRec(n-2, a);
-        
-    //     return  a[n] + max(one, two);
-    // }  
-    
-    
-    // long long solveMem(int n, vector<int> &a, vector<int> &dp){
-    //     if(n<0) return 0;
-    //     if(n==0) return a[n];
-    //     if(dp[n])
-    //         return dp[n];
-    //     long long one = solveMem(n-1, a, dp);
-    //     long long two = solveMem(n-2, a, dp);
-        
-    //     dp[n] = a[n] + max(one, two);
-    //     return dp[n];
-    // }  
-    
-    
-    // long long solveTab(int n, vector<int> &a){
-    //     vector<long long> dp(n+1, 0);
-    //     dp[0] = 0;
-    //     dp[1] = a[0];
-        // for(int i=2; i<=n; i++){
-        //     long long one = dp[i-1];
-        //     long long two = dp[i-2];
-        //     dp[i] = a[i-1] + max(one, two);
-        // }
-        // return dp[n];
-    // }
-    
-    long long solveOPT(int n, vector<int> &a){
-        long long prev1 = a[0];
-        long long prev2 = 0;
-        long long ans;
-        for(int i=1; i<n; i++){
-            ans = a[i] + max(prev1, prev2);
-            prev2 = prev1;
-            prev1 = ans;
+   long long solve(int n, vector<int> &a,int index ,vector<int>&dp ){
+        //base condition
+        if(index>=n){
+            return 0;
         }
-        if(n==1){
-            return prev1;
+        
+        if(dp[index]!=-1){
+            return dp[index];
         }
-        return ans;
-    }
-    
-    
+        long long include = a[index]+solve(n,a,index+1,dp);
+        
+        long long exclude = 0;
+        if(index+1 < n){
+            exclude = a[index+1]+solve(n,a,index+2,dp);
+        }
+        return dp[index] = max(exclude,include);
+        
+    } 
     long long findMaxSubsetSum(int N, vector<int> &A) {
-        vector<int> first;
+       
+        vector<int>dp(N+1,-1);
+        return solve(N,A,0,dp);
         
-        for(int i=0; i<N-1; i++){
-            first.push_back(A[i]);
-        }
-        
-        // return max(solveRec(N-2, first), solveRec(N-1, A));
-        
-        // vector<int> dp(N+1, 0);
-        // return max(solveMem(N-2, first, dp), solveMem(N-1, A, dp));
-        // return max(solveTab(N-1, first), solveTab(N, A));
-        return max(solveOPT(N-1, first), solveOPT(N, A));
     }
 };
 
