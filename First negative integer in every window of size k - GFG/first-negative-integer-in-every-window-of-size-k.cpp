152a0ba4d4c9a674543836ce40157d8fdbaa6477
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-06-05 17:04:35 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,33 +29,30 @@ int main() {
 // } Driver Code Ends
 
 
-vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) {
-                            vector<long long>ans;                     
-                            long long int i=0,j=0;
-                            queue<long long>q;
-                            while(j<N){
-                                if(A[j]<0){
-                                    q.push(A[j]);
-                                }
-                                if((j-i+1)<K){
-                                    j++;
-                                }else if((j-i+1)==K){
-                                    if(q.empty()){
-                                        ans.push_back(0);
-                                    }else{
-                                        ans.push_back(q.front());
-                                        if(q.front()==A[i]){
-                                            q.pop();
-                                        }
-                                        
-                                    }
-                                    i++;
-                                        j++;
-                                }
-                                
-                            }
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
                                                  
+    queue<long long>q;
+    long long r=0;
+    long long l=0;
+vector<long long>ans;
+    while(r<N){
+        if(A[r]<0){
+            q.push(A[r]);
+        }
+        if(r-l+1==K){
+            if(q.empty()){
+                ans.push_back(0);
+            }else{
+                ans.push_back(q.front());
+                if(q.front()==A[l]){
+                q.pop();
+                }
+            }
+            l++;
+        }
+        r++;
+    }
+                return ans;                                 
                                                  
-                                                 
-                                                 return ans;
  }
