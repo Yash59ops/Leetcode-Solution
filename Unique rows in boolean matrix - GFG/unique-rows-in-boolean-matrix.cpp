d50f82e0cea9a10169957ee72e1db3bcfe576ba1
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-06-25 00:01:01 +0530

    Added solution - LeetHub

--- Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
@@ -5,6 +5,35 @@ using namespace std;
 
 vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col);
 
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    // #define MAX 1000
+    vector<vector<int>> uniqueRow(int a[MAX][MAX],int r,int c)
+    {
+        vector<vector<int>>b;
+        map<vector<int>,int>d;
+        for(int i=0;i<r;i++)
+        {
+            vector<int>e;
+            for(int j =0;j<c;j++)
+            {
+                e.push_back(a[i][j]);
+            
+            }
+    
+            if(!d[e]++)
+            b.push_back(e);
+        }
+        
+        return b;
+    }
+};
+
+//{ Driver Code Starts.
+
 int main()
 {
     int T;
@@ -17,7 +46,8 @@ int main()
     	for(int i=0;i<n;i++)
     	for(int j=0;j<m;j++)
     	cin>>a[i][j];
-    	vector<vector<int>> vec = uniqueRow(a,n,m);
+    	Solution ob;
+    	vector<vector<int>> vec = ob.uniqueRow(a,n,m);
     	for(int i = 0;i<vec.size();i++){
     	    for(int x : vec[i]){
     	        cout<<x<<" ";
@@ -29,34 +59,3 @@ int main()
 }
 
 // } Driver Code Ends
-
-
-/*You are required to complete this function*/
-vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
-{
-vector<vector <int>> ans;
-
-    map <vector<int>,int> mp;
-
-    for(int i=0;i<row;i++){
-
-        vector <int> temp;
-
-        for(int j=0;j<col;j++){
-
-            temp.push_back(M[i][j]);
-
-        }
-
-        if(mp.find(temp)==mp.end()){
-
-            mp[temp]=1;
-
-            ans.push_back(temp);
-
-        }
-
-    }
-
-    return ans;
-}
