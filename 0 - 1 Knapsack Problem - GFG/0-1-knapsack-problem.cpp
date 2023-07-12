9fc686e29e9ff5871ae40f2a057765342e130df4
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-12 13:05:14 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,65 +7,30 @@ using namespace std;
 class Solution
 {
     public:
-    int recur(int wt[],int val[],int w,int n,int index){
-        //bc
-        if(index==0){
-             if(wt[index]<=w){
-                 return val[index];
-             }
-             return 0;
-        }
-        int include=0;
-        if(w>=wt[index]){
-            include=val[index]+recur(wt,val,w-wt[index],n,index-1);
-        }
-        int exclude=0+recur(wt,val,w,n,index-1);
-        return max(include,exclude);
-    }
-    int recur_memo(int wt[],int val[],int w,int n,int index,vector<vector<int>>&dp){
-        //bc
-        if(index==0){
-            if(wt[index]<=w)return val[index];
-            return 0;
-        }
-        if(dp[index][w]!=-1)return dp[index][w];
-        int include=0;
-        if(w>=wt[index]){
-            include=val[index]+recur(wt,val,w-wt[index],n,index-1);
-        }
-        int exclude=0+recur(wt,val,w,n,index-1);
-        return dp[index][w]=max(include,exclude);
-    }
+    
     //Function to return max value that can be put in knapsack of capacity W.
+    int help(int total,int wt[],int val[],int n,int index,vector<vector<int>>&dp){
+        //bc
+        if(index==0){
+            if(wt[index]<=total){
+                return dp[index][total]=val[index];
+            }
+            return dp[index][total]=0;
+        }
+        if(dp[index][total]!=-1){
+            return dp[index][total];
+        }
+        int include=0;
+        if(wt[index]<=total){
+            include=val[index]+help(total-wt[index],wt,val,n,index-1,dp);
+        }
+        int exclude=help(total,wt,val,n,index-1,dp);
+        return dp[index][total]=max(include,exclude);
+    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-  //    return recur(wt,val,W,n,n-1);
-    vector<vector<int>>dp(n,vector<int>(W+1,0));
-//    return recur_memo(wt,val,W,n,n-1,dp);    
-      for(int i =wt[0]; i<= W; i++)
-       {
-           if(wt[0] <= W)
-           {
-               dp[0][i] = val[0];
-           }
-           else
-           {
-               dp[0][i] = 0;
-           }
-       }
-       for(int index=1;index<n;index++){
-           for(int cap=0;cap<=W;cap++){
-               int include=0;
-        if(cap>=wt[index]){
-            include=val[index]+dp[index-1][cap-wt[index]];
-        }
-        int exclude=0+dp[index-1][cap];
-        dp[index][cap]=max(include,exclude);
-           }
-       }
-    
-        return dp[n-1][W];
-  
+        vector<vector<int>>dp(n,vector<int>(W+1,-1));
+     return help(W,wt,val,n,n-1,dp);
     }
 };
 
