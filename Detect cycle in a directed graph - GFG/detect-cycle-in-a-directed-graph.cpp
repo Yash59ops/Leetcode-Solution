d0920462f8e58bfcb19c0bbf9af2dc99651b5ff6
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-14 11:39:05 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,62 +6,32 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(vector<int>adj[],int src,vector<bool>&vis,vector<int>&inrecur){
-        vis[src]=true;
-        inrecur[src]=1;
-        for(auto it:adj[src]){
-            if(!vis[it] && dfs(adj,it,vis,inrecur)==true){
+    bool dfs(vector<int>adj[],int u,vector<bool>&vis,vector<bool>&inrecur)
+    {
+        vis[u]=true;
+        inrecur[u]=true;
+        for(auto v:adj[u]){
+            if(vis[v]==true && inrecur[v]==true){
                 return true;
-            }else if(inrecur[it]==true){
-                return true;
+            }else if(!vis[v]){
+                if(dfs(adj,v,vis,inrecur)==true){
+                    return true;
+                }
             }
         }
-        inrecur[src]=0;
+        inrecur[u]=false;
         return false;
     }
     bool isCyclic(int V, vector<int> adj[]) {
-      vector<bool>vis(V,false);
-    /*
-      vector<int>inrecur(V,0);
-    for(int i=0;i<V;i++){
-        if(!vis[i] && dfs(adj,i,vis,inrecur)){
-            return true;
-        }
-    }
-    return false;
-    */
-    vector<int>indeg(V,0);
-	    for(int i=0;i<V;i++){
-	        for(auto u:adj[i]){
-	            indeg[u]++;
-	        }
-	    }
-	    queue<int>q;
-	     int ans=0;
-	    for(int i=0;i<V;i++){
-	       if(indeg[i]==0){
-	           q.push(i);
-	           ans++;
-	       }
-	        }
-	        while(!q.empty()){
-	            int cur=q.front();
-	            q.pop();
-	            for(auto it:adj[cur]){
-	             if(!vis[it]){
-	                 indeg[it]--;
-	                 if(indeg[it]==0){
-	                 q.push(it);
-	                 ans++;
-	                 }
-	             }   
-	            }
-	        }
-	        if(ans==V){
-	            return false;
-	        }else{
-	    return true;
-	        }
+     vector<bool>vis(V,false);
+     vector<bool>inrecur(V,false);
+     for(int i=0;i<V;i++)
+     {
+         if(dfs(adj,i,vis,inrecur)==true){
+             return true;
+         }
+     }
+     return false;
     }
 };
 
