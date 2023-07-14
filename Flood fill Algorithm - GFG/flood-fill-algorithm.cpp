663c44199898b23a1d1542e78b06be79724704b9
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-14 19:00:50 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -5,45 +5,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-void dfs(vector<vector<int>>&arr,vector<vector<int>>&vis,int i,int j,int new_,int old){
-    if(i<0 || j<0 || i>=arr.size() || j>=arr[0].size() || vis[i][j] || arr[i][j]!=old){
-        return;
+    void dfs(int i,int j,vector<vector<int>>&arr,int new_color,vector<vector<int>>&vis,int old_color){
+        if(i<0 || j<0 || i>=arr.size() || j>=arr[0].size() || vis[i][j] || arr[i][j]!=old_color){
+            return;
+        }
+        vis[i][j]=1;
+        arr[i][j]=new_color;
+        dfs(i+1,j,arr,new_color,vis,old_color);
+        dfs(i-1,j,arr,new_color,vis,old_color);
+        dfs(i,j+1,arr,new_color,vis,old_color);
+        dfs(i,j-1,arr,new_color,vis,old_color);
     }
-    vis[i][j]=1;
-    arr[i][j]=new_;
-    dfs(arr,vis,i+1,j,new_,old);
-    dfs(arr,vis,i,j+1,new_,old);
-    dfs(arr,vis,i-1,j,new_,old);
-    dfs(arr,vis,i,j-1,new_,old);
-    
-}
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-       int oldimage=image[sr][sc];
-       int row=image.size();
-       int col=image[0].size();
-       vector<vector<int>>vis(row,vector<int>(col,0));
-    //   dfs(image,vis,sr,sc,newColor,oldimage);
-      queue<pair<int,int>>q;
-      q.push({sr,sc});
-      vis[sr][sc]=1;
-      image[sr][sc]=newColor;
-      vector<pair<int,int>>dirs={{0,-1},{-1,0},{0,1},{1,0}};
-      while(!q.empty()){
-          int x=q.front().first;
-          int y=q.front().second;
-          q.pop();
-          for(auto it:dirs){
-              int x_=x+it.first;
-              int y_=y+it.second;
-            if(x_>=0 && x_<row &&  y_>=0 && y_<col && image[x_][y_]==oldimage && !vis[x_][y_]){
-        image[x_][y_]=newColor;
-        q.push({x_,y_});
-        vis[x_][y_]=1;
-    }
-          }
-      }
-      
-       return image;
+     int row=image.size();
+     int col=image[0].size();
+     int oldcolor=image[sr][sc];
+     vector<vector<int>>vis(row,vector<int>(col,0));
+     dfs(sr,sc,image,newColor,vis,oldcolor);
+     return image;
     }
 };
 
