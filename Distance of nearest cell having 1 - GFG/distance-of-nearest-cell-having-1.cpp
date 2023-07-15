482bc6db5357cbc33e509325b3aeab0171fc0a6b
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-15 11:39:25 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -11,42 +11,38 @@ class Solution
 	{
 	    int row=grid.size();
 	    int col=grid[0].size();
-	  queue<pair<pair<int,int>,int>>q;
-	  vector<vector<int>>dist(row,vector<int>(col,0));
-	  vector<vector<int>>vis(row,vector<int>(col,0));
-	  for(int i=0;i<row;i++){
-	      for(int j=0;j<col;j++){
-	          if(grid[i][j]==1){
-	              q.push({{i,j},0});
-	              vis[i][j]=1;
-	          }
-	      }
-	  }
-	
-	  vector<pair<int,int>>dirs={{-1,0},{0,-1},{1,0},{0,1}};
-	  while(!q.empty()){
-	     int x=q.front().first.first;
-	     int y=q.front().first.second;
-	     int d=q.front().second;
-	      q.pop();
-	      dist[x][y]=d;
-	
-	      for(int i=0;i<4;i++){
-	          
-	          int x_=x+dirs[i].first;
-	          
-	          int y_=y+dirs[i].second;
-	          
-	          if(x_>=0 &&  x_<row && y_>=0  && y_<col && vis[x_][y_]==0 && grid[x_][y_]==0){
-	              q.push({{x_,y_},d+1});
-	              vis[x_][y_]=1;
-	           
-	          }
-	      }
-	      
-	  }
-	
-	  return dist;
+	   vector<vector<int>>ans(row,vector<int>(col,0));
+	   vector<vector<int>>vis(row,vector<int>(col,0));
+      queue<pair<pair<int,int>,int>>q;
+	   for(int i=0;i<row;i++){
+	       for(int j=0;j<col;j++){
+	           if(grid[i][j]==1){
+	               q.push({{i,j},0});
+	               vis[i][j]=1;
+	           }
+	       }
+	   }
+	   vector<pair<int,int>>dirs={{-1,0},{1,0},{0,-1},{0,1}};
+	   while(!q.empty()){
+	       int x=q.front().first.first;
+	       int y=q.front().first.second;
+	       int d=q.front().second;
+	       q.pop();
+	       ans[x][y]=d;
+	       for(auto it:dirs){
+	           int x_=x+it.first;
+	           int y_=y+it.second;
+	           if(x_>=0 && y_>=0 && x_<row && y_<col && !vis[x_][y_] && grid[x_][y_]==0){
+	               q.push({{x_,y_},d+1});
+	               vis[x_][y_]=1;
+	           }
+	       }
+	   }
+	   
+	   
+	   
+	   return ans;
+	   
 	}
 };
 
