6010760d3722495fc8a7c1d2852884be7dfc7425
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-17 00:40:29 +0530

    Added solution - LeetHub

--- Subarrays with sum K - GFG/subarrays-with-sum-k.cpp
+++ Subarrays with sum K - GFG/subarrays-with-sum-k.cpp
@@ -7,17 +7,18 @@ using namespace std;
 
 class Solution{
     public:
-    int findSubArraySum(int nums[], int N, int k)
+    int findSubArraySum(int Arr[], int N, int k)
     {
         unordered_map<int,int>mp;
+        int prefix=0;
         mp[0]=1;
-        int ans=0,sum=0;
+        int ans=0;
         for(int i=0;i<N;i++){
-            sum+=nums[i];
-            if(mp.find(sum-k)!=mp.end()){
-                ans+=mp[sum-k];
+            prefix+=Arr[i];
+            if(mp.find(prefix-k)!=mp.end()){
+                ans+=mp[prefix-k];
             }
-            mp[sum]++;
+            mp[prefix]++;
         }
         return ans;
     }
