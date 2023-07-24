883e6f2baf62dba0d4bedb6bacb486447b779ce5
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-07-25 00:04:32 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,30 +5,63 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to detect cycle in an undirected graph.
-    bool dfs(vector<int>adj[],int u,int parent,vector<bool>&visited){
-        visited[u]=true;
-        for(auto v:adj[u]){
+    bool dfs(vector<int>adj[],int u,vector<bool>&vis,int parent){
+        vis[u]=true;
+        for(auto &v:adj[u]){
             if(v==parent){
                 continue;
             }
-            if(visited[v]){
+            if(vis[v]==true){
                 return true;
             }
-            if(dfs(adj,v,u,visited)==true){
+            if(dfs(adj,v,vis,u)==true){
                 return true;
+            }
+        }
+        return false;
+        
+    }
+    bool isCycleBFS(int src, vector<int>* adj, vector<bool>& visited) {
+        unordered_map<int, int> parent;
+        parent[src] = -1;
+        visited[src] = true;
+        queue<int> q;
+        q.push(src);
+        
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            
+            for(auto neighbour : adj[front]) {
+                if(visited[neighbour] == true && neighbour != parent[front]) {
+                    return true;
+                }
+                else if(!visited[neighbour]) {
+                    q.push(neighbour);
+                    visited[neighbour] = true;
+                    parent[neighbour] = front;
+                }
             }
         }
         return false;
     }
+    
     bool isCycle(int V, vector<int> adj[]) {
-        vector<bool>vis(V,false);
-        for(int i=0;i<V;i++){
-            if(!vis[i] && dfs(adj,i,-1,vis)){
-                return true;
-            }
+    vector<bool>vis(V,false);
+/*
+    for(int i=0;i<V;i++){
+        if(!vis[i] && dfs(adj,i,vis,-1)){
+            return true;
         }
-        return false;
+    }
+    return false;
+    */
+    for(int i=0;i<V;i++){
+        if(!vis[i] && isCycleBFS(i,adj,vis)){
+            return true;
+        }
+    }
+    return false;
     }
 };
 
