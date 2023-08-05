9168ab67c3bbb6ae74689f3cad71dab7ce88a266
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-08-05 11:20:00 +0530

    Added solution - LeetHub

---
+++ Substrings with same first and last characters - GFG/substrings-with-same-first-and-last-characters.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+
+	int countSubstringWithEqualEnds(string s)
+	{
+	         unordered_map<char,int> mp;
+       int count=0;
+       for(int i=0;i<s.length();i++)
+       {
+            mp[s[i]]++;
+           count+=mp[s[i]];
+       }
+       return count;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string str;
+   		cin >> str;
+
+   	    Solution ob;
+
+   		cout << ob.countSubstringWithEqualEnds(str) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
