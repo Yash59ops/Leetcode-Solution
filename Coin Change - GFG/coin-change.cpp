e8327d968236a7045f43337cf6e47803bdd598cf
Yash Mehar <72158748+Yash59ops@users.noreply.github.com>
2023-08-11 15:33:21 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -20,11 +20,40 @@ class Solution {
        not_pick=recur(coins,n,sum,index+1,dp);
        return dp[index][sum]=pick+not_pick;
   }
-    long long int count(int coins[], int N, int sum) {
-vector<vector<long long int>>dp(N+1,vector<long long int>(sum+1,-1));
-        return recur(coins,N,sum,0,dp);
+    long long int count(int a[], int n, int value) {
+   vector<vector<long long int >> dp(n,vector<long long int >(value +1,0));
+        vector<long long int> prev(value+1,0), curr(value+1,0);
+        for(int t=0;t<=value;t++){
+            // dp[0][t] = (t % a[0] ==0 );
+            prev[t] = (t % a[0] ==0 );
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int T=0;T<=value;T++){
+                /*long long int notTake = dp[ind-1][T];
+                long long int Take = 0; 
+                
+                if(a[ind]<=T) Take = dp[ind][T-a[ind]];
+                
+                dp[ind][T] = Take+notTake;*/
+                
+                //space optimization  
+                
+                long long int notTake = prev[T];
+                long long int Take = 0; 
+                
+                if(a[ind]<=T) Take = curr[T-a[ind]];
+                
+                curr[T] = Take+notTake;
+                
+            }
+            prev= curr;
+        }
+        
+        return prev[value];
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
